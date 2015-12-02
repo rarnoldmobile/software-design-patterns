be93e84f14574049eefb61134b9fccb0f8a796a3
Robert Arnold <rarnoldmobile@gmail.com>
2015-12-01 22:19:30 -0500

    A handful of functional examples

--- techniques/functional-patterns/src/main/java/com/example/rarnoldmobile/App.java
+++ techniques/functional-patterns/src/main/java/com/example/rarnoldmobile/App.java
@@ -1,13 +1,45 @@
 package com.example.rarnoldmobile;
 
-/**
- * Hello world!
- *
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.example.rarnoldmobile.classes.*;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<School> schoolList = new ArrayList<School>();
+        
+        schoolList.add(new School("test School1", 100, 10));
+        schoolList.add(new School("test School2", 200, 30));
+        schoolList.add(new School("test School3", 300, 20));
+        schoolList.add(new School("test School4", 120, 8));
+        schoolList.add(new School("test School5", 405, 35));
+        
+        
+        long school300plus = 
+        	schoolList
+        		.stream()
+        		.distinct()
+        		.filter(school -> school.getStudentCount() >= 300 )
+        		.count();
+        
+        
+        List<String> schoolNames = schoolList
+                .stream()
+                .flatMap(school -> Stream.of(school.getSchoolName()))
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
+
+        
+        System.out.println("School count with more than 299 students: " + school300plus);
+        System.out.println("School names: " + schoolNames);
+        
+        
     }
 }
